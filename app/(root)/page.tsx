d0705c3081d135e0d09b8c0e48136652f2d3ab9c
Arthur Dias <dias.arthurr@gmail.com>
2023-10-02 16:07:57 -0300

    Added modal to root page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,21 @@
-import { UserButton } from '@clerk/nextjs'
+'use client'
+
+import { useEffect } from 'react'
+import { useStoreModal } from '@/hooks/useStoreModal'
 
 const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen)
+  const isOpen = useStoreModal((state) => state.isOpen)
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen()
+    }
+  }, [isOpen, onOpen])
+
   return (
     <div className='p-5'>
-      <UserButton afterSignOutUrl='/' />
-      <p>Admin Dashboard</p>
+      <p>Root page</p>
     </div>
   )
 }
